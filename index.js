8c6932bd8a985548ddfa65cd1d110ad805ed8bec
azizur2003 <144415011+azizur2003@users.noreply.github.com>
2023-09-08 19:26:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#element', {
+    strings: ['Freelancer', 'Web Designer', ' Frontend Developer.'],
+    typeSpeed: 50,
+});
+
+
+$(".account").on("click", function () {
+    $(this).fadeIn(100).fadeOut(100).fadeIn(100);
+});
